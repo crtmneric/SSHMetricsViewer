96ca495c313cc31c1f4fdedae527d9fc0492c9c5
Serkan Özdemir <s3rkanodemir@gmail.com>
2018-06-21 16:54:37 +0300

    SNAPSHOT dependency added

--- src/main/java/com/pribas/controller/ConnectionController.java
+++ src/main/java/com/pribas/controller/ConnectionController.java
@@ -1,24 +1,21 @@
 package com.pribas.controller;
 
 
-import com.jcraft.jsch.Session;
 import com.pribas.beans.MetricsReader;
 
 import java.io.Console;
-import java.util.Arrays;
 import java.util.Scanner;
 
 public class ConnectionController {
     public static void main(String[] args) {
-        String[] str = new String[] {
+        String[] str = new String[]{
                 "0000     0      0000  0   0  0000      0000   0000   0    0   ",
                 "0       0 0    0       0 0  0         0      0       0    0  ",
                 "000    00000    0000    0    0000      0000   0000   000000          ",
                 "0     0     0       0   0        0         0      0  0    0        ",
                 "0000 0       0  0000    0    0000      0000   0000   0    0         "};
 
-        for (int i=0;i<str.length;i++)
-        {
+        for (int i = 0; i < str.length; i++) {
             System.out.println(str[i]);
         }
         System.out.println();
@@ -42,10 +39,10 @@ public class ConnectionController {
         System.out.println("-----------------------MEMORY USAGE-----------------------");
         MetricsReader reader = new MetricsReader();
 
-        reader.execCommand("free -m",reader.sessionSSH(username,pass,host,port));
+        reader.execCommand("free -m", reader.sessionSSH(username, pass, host, port));
         System.out.println("-----------------------DISK USAGE-------------------------");
 
-        reader.execCommand("df -h",reader.sessionSSH(username,pass,host,port));
+        reader.execCommand("df -h", reader.sessionSSH(username, pass, host, port));
     }
 
 
